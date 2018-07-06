ab2f61793447297b1a5b88e5bd5c1185667b5713
Rémy HUBSCHER <remy@chefclub.tv>
2018-07-06 18:02:14 +0200

    Display the first image.

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,8 @@ pub struct Model {
 
 pub enum Msg {
     FetchData,
-    FetchReady(Result<String, Error>),
+    FetchDataReady(Result<String, Error>),
+    FetchImageReady(Result<String, Error>),
     Ignore,
 }
 
@@ -31,6 +32,12 @@ pub struct DataFromFile {
 #[derive(Deserialize, Debug)]
 pub struct ImageFromFile {
     url: String
+}
+
+/// This type is used to parse data from the Github API
+#[derive(Deserialize, Debug)]
+pub struct ContentFromFile {
+    content: String
 }
 
 impl<CTX> Component<CTX> for Model
@@ -56,7 +63,7 @@ where
                     let (meta, Json(data)) = response.into_parts();
                     println!("META: {:?}, {:?}", meta, data);
                     if meta.status.is_success() {
-                        Msg::FetchReady(data.map(|c| c.tree[0].url.clone()))
+                        Msg::FetchDataReady(data.map(|c| c.tree[4].url.clone()))
                     } else {
                         Msg::Ignore  // FIXME: Handle this error accordingly.
                     }
@@ -69,7 +76,25 @@ where
                 let task = fetch_service.fetch(request, callback);
                 self.ft = Some(task);
             }
-            Msg::FetchReady(response) => {
+            Msg::FetchDataReady(response) => {
+                let callback = env.send_back(move |response: Response<Json<Result<ContentFromFile, Error>>>| {
+                    let (meta, Json(data)) = response.into_parts();
+                    println!("META: {:?}, {:?}", meta, data);
+                    if meta.status.is_success() {
+                        Msg::FetchImageReady(data.map(|c| c.content.clone()))
+                    } else {
+                        Msg::Ignore  // FIXME: Handle this error accordingly.
+                    }
+                });
+                let request = Request::get(response.map(|data| data).ok().unwrap())
+                    .header("Authorization", "c8b0db79c202ad52e0ba64a39f2e44fb4f41c543")
+                    .body(Nothing)
+                    .unwrap();
+                let fetch_service: &mut FetchService = env.as_mut();
+                let task = fetch_service.fetch(request, callback);
+                self.ft = Some(task);
+            }
+            Msg::FetchImageReady(response) => {
                 self.fetching = false;
                 self.data = response.map(|data| data).ok();
             }
@@ -105,7 +130,7 @@ impl Model {
     {
         if let Some(ref value) = self.data {
             html! {
-                <p>{ format!("data:image/jpg;base64,{}", value) }</p>
+                <p><img src=format!("data:image/jpeg;base64,{}", str::replace(&value, "\n", "")), /></p>
             }
         } else {
             html! {
